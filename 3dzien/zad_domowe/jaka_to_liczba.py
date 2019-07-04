775be3b1d6b8f260c8fab49ecbc746ae6e32b2be
Krzysiek <bojakki@gmail.com>
2019-07-04 21:44:13 +0200

    Gra_jaka to liczba

--- 3dzien/zad_domowe/jaka_to_liczba.py
+++ 3dzien/zad_domowe/jaka_to_liczba.py
@@ -54,6 +54,7 @@
 import random
 
 print("Zgadnij jaką liczbę zakresu od 1-100 dla Ciebie mam...")
+print("Masz tylko 7 prób")
 liczba = random.randint(1, 101)
 zgadl = False
 
@@ -62,19 +63,22 @@ x = 1
 while not zgadl:   #rozwin warunek dla while
     zgadywana = int(input("podaj liczbe: ")) 
 
-    x =+ x + 1
+    x += 1
 
     if zgadywana == liczba:   
         print("Brawo, udało Ci się")
-    
+        break
+
+    elif x == 7:
+        print("Game Over")
+        break
+
+
     elif zgadywana > liczba:
         print("za dużo")
     
     elif zgadywana < liczba:
         print("za mało")
-        elif x = 7:
-            print("Game Over")
-        break
 
 
 
